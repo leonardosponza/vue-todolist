25d4c63d8e1670420d1d06beb4cd1791e3d4966a
leonardo <sponzal4@gmail.com>
2021-06-23 17:31:45 +0200

    finex

--- js/script.js
+++ js/script.js
@@ -1,13 +1,35 @@
-const app = new Vue ({
-    el : '#app',
-    data:{
-        todos : [
-            'pomiciare',
-            'bere',
-            'drogarsi',
-            'studiare',
-            'dormire'
-        ]
-
+const app = new Vue(
+    {
+        el : '#app',
+        data : {
+            todoList : [
+                'pomiciare',
+                'drogarsi',
+                'dormire',
+                'mangiare'
+            ],
+            newTodo : ''
+        },
+        methods: {
+            addnewtodo : function(){
+                if(this.newTodo != ''){
+                    this.todoList.push(this.newTodo);
+                    this.newTodo = '';
+                }
+            },
+            removeTodo : function(index){
+                this.todoList.splice(index,1);
+            },
+            remove : function(){
+                let userAnswer = confirm('rimuovi la lista');
+                if (userAnswer) {
+                    this.todoList = [];
+                }
+            },
+            addClass : function(indice){
+                let element = document.getElementById(indice);
+                element.classList.toggle('text-bar');
+            }
+        }
     }
-});
+);
