cbad23aeb51ea988f1d8b95c592187ea0bae99a3
leonardo <sponzal4@gmail.com>
2021-06-23 17:06:04 +0200

    change

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const app = new Vue ({
+    el : '#app',
+    data:{
+        todos : [
+            'pomiciare',
+            'bere',
+            'drogarsi',
+            'studiare',
+            'dormire'
+        ]
+
+    }
+});
